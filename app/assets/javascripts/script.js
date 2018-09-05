082fd78ebcbb66a261aea3ee3fcab7d97ad48016
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-05 23:27:40 +0900

    add view for recipe detail upto date

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,11 +1,11 @@
 $(function() {
-  $('.bigRecipes').slick({
+  $('.bigThumRecipes').slick({
     infinite: true,
     slidesToShow: 2,
     centerMode: true, //要素を中央寄せ
     centerPadding:'50px', //両サイドの見えている部分のサイズ
     autoplay:true, //自動再生
-    autoplaySpeed : 2200,
+    autoplaySpeed : 2000,
     dots: true,
   })
 });
