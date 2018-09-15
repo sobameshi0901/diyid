803c521bf94d5f39d57101d26199e9b0218fee1c
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-15 23:45:56 +0900

    Add recipe registration function only detail

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :category, optional: true
   has_many :steps
   has_many :materials
+  accepts_nested_attributes_for :steps, :materials
   mount_uploader :image, ImageUploader
 end
