27fd00859031e8091f81146338d372d49c9939db
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-24 22:14:28 +0900

    Add route for material & step under recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,20 +7,17 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.materials.build
-    @recipe.steps.build
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
-    binding/
-    @recipe.save
+    if @recipe.save
     render :new
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :comment, :build_time, :cost, :size, :point,:image, :category_id, materials_attributes:[:name, :category],steps_attributes: [:content, :image],)
+    params.require(:recipe).permit(:name, :comment, :build_time, :cost, :size, :point,:image, :category_id, materials_attributes:[:name, :category],steps_attributes: [:content, :image])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'recipes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :recipes, only:[:index, :show, :new, :create]
+  resources :recipes, only:[:index, :show, :new, :create] do
+    resources :materials, only:[:new, :create]
+    resources :steps, only: [:new, :create]
+  end
 end
