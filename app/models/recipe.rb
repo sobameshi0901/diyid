a07d7d301fe876770b4e137823f786285835796e
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-26 12:38:37 +0900

    Add validation on Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :category, optional: true
   has_many :steps
   has_many :materials
+  validates :name, :image, :comment, :category, presence: true
   accepts_nested_attributes_for :steps, allow_destroy: true, reject_if: proc { |attributes| attributes['content'].blank? }
   accepts_nested_attributes_for :materials, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? }
   
