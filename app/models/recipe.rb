5004ec456b132a63509d48944e43bbe62e87eaad
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-26 22:49:18 +0900

    Change validation for nested attributes to delete when column is empty for update method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,25 @@ class Recipe < ApplicationRecord
   has_many :steps
   has_many :materials
   validates :name, :image, :comment, :category, presence: true
-  accepts_nested_attributes_for :steps, allow_destroy: true, reject_if: proc { |attributes| attributes['content'].blank? }
-  accepts_nested_attributes_for :materials, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? }
+  accepts_nested_attributes_for :steps, allow_destroy: true, reject_if: :reject_steps
+  # proc { |attributes| attributes['content'].blank? }
+  accepts_nested_attributes_for :materials, allow_destroy: true, reject_if: :reject_materials
+  # proc { |attributes| attributes['name'].blank? }
   
   mount_uploader :image, ImageUploader
+
+  def reject_steps(attributes)
+    exists = attributes[:id].present?
+    empty = attributes[:content].blank?
+    attributes.merge!(_destroy: 1) if exists && empty
+    !exists && empty
+  end
+
+  def reject_materials(attributes)
+    exists = attributes[:id].present?
+    empty = attributes[:name].blank?
+    attributes.merge!(_destroy: 1) if exists && empty
+    !exists && empty
+  end
+
 end
