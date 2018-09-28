d6a1011a8315969a3131bb30f77a9dacfc7f2025
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-28 20:16:40 +0900

    Add avatar to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: {medium: "300x300#", humb: "100x100#"}
+  validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png"]
 end
