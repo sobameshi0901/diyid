fc1bb89008beaf60953471ff4413004425278fcd
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-29 10:19:00 +0900

    Delete unnecessary method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_out_path_for(resource)
-    '/users/sign_in' # サインアウト後のリダイレクト先URL
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :avatar])
