98173be1482f53f71406cd7e0285a6fb293e46e3
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-29 10:18:32 +0900

    Merge migration file

--- db/migrate/20180928100030_add_attachment_avatar_to_users.rb
+++ db/migrate/20180928100030_add_attachment_avatar_to_users.rb
@@ -2,10 +2,12 @@ class AddAttachmentAvatarToUsers < ActiveRecord::Migration[5.2]
   def self.up
     change_table :users do |t|
       t.attachment :avatar
+      t.string :nickname
     end
   end
 
   def self.down
     remove_attachment :users, :avatar
+    remove_column :users, :nickname
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -70,11 +70,11 @@ ActiveRecord::Schema.define(version: 2018_09_28_100030) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
