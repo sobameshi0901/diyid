5cdb0b3d0ec52f77b8c44aca87503e1d30d8ce12
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-01 00:07:54 +0900

    Define create and delete methods for favorite controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,10 @@
 class FavoritesController < ApplicationController
+  def create
+    @favorite = Favorite.create(recipe_id: params[:recipe_id], user_id: current_user.id)
+  end
+
+  def destroy
+    favorite = Favorite.find(params[:id])
+    favorite.delete
+  end
 end
