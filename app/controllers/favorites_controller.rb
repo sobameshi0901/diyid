cbcf6fdbbb1db76e9cc45ee5da76d1f28859c9d3
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-01 00:40:22 +0900

    Add redirect action after favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,12 @@
 class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.create(recipe_id: params[:recipe_id], user_id: current_user.id)
+    redirect_to recipe_path(params[:recipe_id])
   end
 
   def destroy
     favorite = Favorite.find(params[:id])
     favorite.delete
+    redirect_to recipe_path(params[:recipe_id])
   end
 end
