6c443a056680350602366222cefc77672a3b0a72
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-02 21:29:19 +0900

    Revise indent

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,9 @@
 $(function() {
   $(".tabLabel").on("click",function(){
-  var $th = $(this).index();
-  $(".tabLabel").removeClass("active");
-  $(".tabPanel").removeClass("active");
-  $(this).addClass("active");
-  $(".tabPanel").eq($th).addClass("active");
+    var $th = $(this).index();
+    $(".tabLabel").removeClass("active");
+    $(".tabPanel").removeClass("active");
+    $(this).addClass("active");
+    $(".tabPanel").eq($th).addClass("active");
   });
 });
