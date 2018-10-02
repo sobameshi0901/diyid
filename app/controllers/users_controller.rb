12b8f46c561eddb1ca6694163bd354f7029c65be
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-02 21:27:22 +0900

    Change writing way for map method on user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @favo_recipes = current_user.favorites.map{|favo| favo.recipe} if current_user.favorites
+    @favo_recipes = current_user.favorites.map(&:recipe) if current_user.favorites
     @post_recipes = current_user.recipes.includes(:user) if current_user.recipes
   end
 end
