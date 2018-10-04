241340fda1656923387858514ffa2218694dc0ce
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-04 13:41:20 +0900

    Add foreign_key true

--- db/migrate/20180930122641_createfavorites.rb
+++ db/migrate/20180930122641_createfavorites.rb
@@ -1,8 +1,8 @@
 class Createfavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.references :user, null: false
-      t.references :recipe, null: false
+      t.references :user, null: false, foregin_key: true
+      t.references :recipe, null: false, foregin_key: true
       t.timestamps
 
       t.index [:user_id, :recipe_id], unique: true
