16591c56fe418c19ad9d5e20c5b7444117930308
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-04 17:33:28 +0900

    Fix remote favorite function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,13 @@
 class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.create(recipe_id: params[:recipe_id], user_id: current_user.id)
-    redirect_to recipe_path(params[:recipe_id])
+    @recipe = Recipe.find(params[:recipe_id])
   end
 
   def destroy
-    favorite = Favorite.find(params[:id])
-    favorite.delete
-    redirect_to recipe_path(params[:recipe_id])
+    @favorite = Favorite.find(params[:id])
+    @favorite.delete
+    @favorite = nil
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
