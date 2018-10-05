0166702fb3de93a9c80ac8db93e0d0b6b1484617
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-05 22:21:14 +0900

    user. js change on Oct 5

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,9 @@
-$(document).on('turbolinks:load',function() {
-  $(".tabLabel").on("click",function(){
-    var $th = $(this).index();
-    $(".tabLabel").removeClass("active");
-    $(".tabPanel").removeClass("active");
-    $(this).addClass("active");
-    $(".tabPanel").eq($th).addClass("active");
-  });
-});
+// $(document).on('turbolinks:load',function() {
+//   $(".tabLabel").on("click",function(){
+//     var $th = $(this).index();
+//     $(".tabLabel").removeClass("active");
+//     $(".tabPanel").removeClass("active");
+//     $(this).addClass("active");
+//     $(".tabPanel").eq($th).addClass("active");
+//   });
+// });
