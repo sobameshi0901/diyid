81b2f9c14c52f9413b004ebc06216b52baae94c9
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-07 20:39:32 +0900

    Delete user js

--- app/assets/javascripts/user.js
+++
@@ -1,9 +0,0 @@
-// $(document).on('turbolinks:load',function() {
-//   $(".tabLabel").on("click",function(){
-//     var $th = $(this).index();
-//     $(".tabLabel").removeClass("active");
-//     $(".tabPanel").removeClass("active");
-//     $(this).addClass("active");
-//     $(".tabPanel").eq($th).addClass("active");
-//   });
-// });
