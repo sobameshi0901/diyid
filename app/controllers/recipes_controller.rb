f94ca02240a4ca92b8dee35b88f9973ac9c1ec70
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-09 20:18:54 +0900

    Add validation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,6 +24,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+    binding.pry
     if @recipe.save
       redirect_to @recipe
     else
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :tag_maps dependent: :destroy
+  has_many :tag_maps, dependent: :destroy
   has_many :recipes, through: :tag_maps
+  validates :name, presence: true
 end
