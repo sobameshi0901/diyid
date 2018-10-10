a94355bef78d5fffc46265c05c7b4c4a6e6dda0e
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-10 23:52:46 +0900

    Define method for register Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,13 @@ class Tag < ApplicationRecord
   has_many :tag_maps, dependent: :destroy
   has_many :recipes, through: :tag_maps
   validates :name, presence: true
+
+  def self.register_tag(params)
+    params.each do |tag_array|
+      # tag_arrayは[ 0, "タグの名前" ]が入っている
+      unless Tag.exists?(name: tag_array[1]) || tag_array[1] == nil
+        Tag.create(name: tag_array[1])
+      end
+    end
+  end
 end
