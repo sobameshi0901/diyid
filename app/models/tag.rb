040bb34d706ac8cc19a2c99a40ae17ab4ef04a2b
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-11 00:29:05 +0900

    Fix set_tag method on tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,12 +3,17 @@ class Tag < ApplicationRecord
   has_many :recipes, through: :tag_maps
   validates :name, presence: true
 
-  def self.register_tag(params)
+  def self.set_tags(params)
+    tag_lists = []
     params.each do |tag_array|
       # tag_arrayは[ 0, "タグの名前" ]が入っている
-      unless Tag.exists?(name: tag_array[1]) || tag_array[1] == nil
-        Tag.create(name: tag_array[1])
+      tag_name = tag_array[1]
+      unless tag_name == nil
+        tag = Tag.where(name: tag_name).first_or_initialize
+        tag.save
       end
+      tag_lists << tag
     end
+    return tag_lists
   end
 end
