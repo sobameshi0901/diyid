cf4e3c055b6340dfafb254767cba866c8ce6fc1a
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-14 16:41:43 +0900

    Divide method for message in case of new and reply

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,21 @@
 class MessagesController < ApplicationController
   def create
-    if params[:message][:status] == 'new'
-      @question = Question.new(recipe_id: params[:recipe_id])
-      @question.save
-      @status = check_status
-      message = Message.new(message_params)
-      message.save
-      redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
-    else
-      @status = check_status
-      message = Message.new(message_params)
-      message.save
-      redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
-    end
+    # このアクションに送られてくるのは新しいquestionなので、questionをnew&saveする。
+    @question = Question.new(recipe_id: params[:recipe_id])
+    @question.save
+    # formで送られたparams+replyアクションと共通のカラムをセットするmessage_paramsに、最初の質問に対するis_firstに0を引数として渡してセット。
+    message = Message.new(message_params('0'))
+    message.save
+    redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
   end
+
 
   private
-  def message_params
-    params.require(:message).permit(:context).merge(user_id: current_user.id, question_id: @question.id, status: @status)
+  def message_params(is_first)
+    params.require(:message).permit(:context).merge(user_id: current_user.id, question_id: @question.id, is_recipe_user: check_status, is_first: is_first)
   end
 
+  # messageの投稿者がrecipeの投稿者と同じか判定
   def check_status
     if current_user.id == Recipe.find(params[:recipe_id]).user_id
       return '0'
