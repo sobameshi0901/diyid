9793f0d1f35a2ec0662710c191dbc13add1bbec9
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-10-16 21:43:55 +0900

    Add function when fail post

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,28 @@ class MessagesController < ApplicationController
   def create
     # このアクションに送られてくるのは新しいquestionなので、questionをnew&saveする。
     @question = Question.new(recipe_id: params[:recipe_id])
-    @question.save
-    # formで送られたparams+replyアクションと共通のカラムをセットするmessage_paramsに、最初の質問に対するis_firstに0を引数として渡してセット。
-    message = Message.new(new_message_params)
-    message.save
-    redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
+    if @question.save
+      # formで送られたparams+replyアクションと共通のカラムをセットするmessage_paramsに、最初の質問に対するis_firstに0を引数として渡してセット。
+      message = Message.new(new_message_params)
+      if message.save
+        redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
+      else
+        @question.delete
+        redirect_recipe_when_fail('質問の投稿に失敗しました')
+      end
+    else
+      redirect_recipe_when_fail('質問の投稿に失敗しました')
+    end
   end
 
   def reply
     @question = Question.find(params[:question_id])
     message = Message.new(reply_message_params)
-    message.save
-    redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
+    if message.save
+      redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
+    else
+      redirect_recipe_when_fail('質問への返信に失敗しました')
+    end
   end
 
   def destroy
@@ -23,6 +33,8 @@ class MessagesController < ApplicationController
       # messageの親であるquestionを削除。子のmessagesも一緒にdestroyされる
       message.question.destroy
       redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
+    else
+      redirect_recipe_when_fail('投稿の削除に失敗しました')
     end
   end
 
@@ -42,5 +54,10 @@ class MessagesController < ApplicationController
     else
       return '1'
     end
+  end
+
+  def redirect_recipe_when_fail(message)
+    redirect_to controller: :recipes, action: :show, id: params[:recipe_id]
+    flash[:danger] = message
   end
 end
